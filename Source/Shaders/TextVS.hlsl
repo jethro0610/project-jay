1bd3182bca216fce9923e4cb007e7f66e5eff696
JetDesktop <jethro0610@outlook.com>
2023-03-27 20:33:21 -0700

    Auto-size text vertices based on kerning

--- Source/Shaders/TextVS.hlsl
+++ Source/Shaders/TextVS.hlsl
@@ -5,7 +5,10 @@
 static const int CHARS_PER_LINE = 32;
 static const int MAP_CHARS = 16;
 static const float CHAR_SIZE = 1.0f / float(MAP_CHARS);
-static const float KERNING = 0.3f;
+static const float KERNING = 0.5f;
+static const float HEIGHT = 0.125f;
+static const float KERN_WIDTH = 1.0f - KERNING;
+static const float MARGIN = 0.05f;
 
 TextOut main(TextIn inputText) {
     TextOut output;
@@ -13,19 +16,18 @@ TextOut main(TextIn inputText) {
     int curChar = inputText.instanceID % CHARS_PER_LINE;
 
     float inverseRatio = 1.0f / aspectRatio;
-    float marginY = 0.05f;
-    float marginX = marginY * inverseRatio;
+    float marginX = MARGIN * inverseRatio;
 
-    float height = 0.1f;
-    float width = height * inverseRatio;
+    float width = KERN_WIDTH * HEIGHT * inverseRatio;
 
     float2 coord = float2(inputText.vertexID&1, inputText.vertexID>>1);
     float originX = coord.x * width - 1.0f;
-    float originY = -coord.y * height + 1.0f;
-    float posX = originX + (width - KERNING * height) * curChar + marginX;
-    float posY = originY - height * curLine - marginY;
+    float originY = -coord.y * HEIGHT + 1.0f;
+    float posX = originX + width * curChar + marginX;
+    float posY = originY - HEIGHT * curLine - MARGIN;
 
     output.coord = coord * CHAR_SIZE;
+    output.coord.x *= KERN_WIDTH;
     /* output.coord.x += CHAR_SIZE; */
     int charRow = floor(inputText.charIndex / MAP_CHARS);
     int charCol = inputText.charIndex % MAP_CHARS;

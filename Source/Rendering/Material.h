653caa6d77fee84a56d8bebccc01e6284c2c6702
Jet <jethro0610@outlook.com>
2023-07-25 11:20:42 -0700

    Remove material constructor

--- Source/Rendering/Material.h
+++ Source/Rendering/Material.h
@@ -8,9 +8,4 @@ struct Material {
     Texture textures[MAX_TEXTURES_PER_MATERIAL];
     int numTextures;
     bool twoSided;
-
-    Material() {
-        numTextures = 0;
-        twoSided = false;
-    }
 };

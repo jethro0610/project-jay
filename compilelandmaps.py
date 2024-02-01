61a6fbce7b50dd6e74c3058fd4d3a99243d0cee9
Jethro Peralta <jethro0610@outlook.com>
2024-01-31 20:57:48 -0800

    Add landmap files

---
+++ compilelandmaps.py
@@ -0,0 +1,19 @@
+import subprocess
+import os
+from termcolor import colored
+
+input_dir = './Assets/landmaps/'
+output_dir = './Output/landmaps/'
+
+def compile_landmap(input_path, output_path):
+    command = 'convert -flatten -resize 1024x1024 ' + input_path + ' GRAY:' + output_path 
+    print(colored("Compiling landmap %s..." % input_path, "yellow"))
+    subprocess.run(command, shell = True)
+
+files = os.listdir(input_dir)
+for fname in files:
+    input_path = os.path.join(input_dir, fname)
+    output_path = os.path.join(output_dir, os.path.splitext(fname)[0] + '.blb')
+
+    if not os.path.exists(output_path) or os.path.getmtime(input_path) > os.path.getmtime(output_path):
+        compile_landmap(input_path, output_path)

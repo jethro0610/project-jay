cc8f85fe9fe4f94de0cbb4cd03745751ca466756
Jet <jethro0610@outlook.com>
2024-07-01 15:21:19 -0700

    Use magick on landmap convert

--- compilelandmaps.py
+++ compilelandmaps.py
@@ -1,6 +1,5 @@
 import subprocess
 import os
-import platform
 from termcolor import colored
 
 input_dir = './Assets/landmaps/'
@@ -8,15 +7,9 @@ output_dir = './Output/landmaps/'
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
-convert_cmd = ''
-if platform.system() == 'Windows':
-    convert_cmd = 'magick'
-elif platform.system() == 'Linux':
-    convert_cmd = 'convert'
-
 
 def compile_landmap(input_path, output_path):
-    command = convert_cmd + ' ' + input_path + ' -flatten -resize 1024x1024 ' + ' GRAY:' + output_path 
+    command = 'magick ' + input_path + ' -flatten -resize 1024x1024 ' + ' GRAY:' + output_path 
     print(colored("Compiling landmap %s..." % input_path, "yellow"))
     subprocess.run(command, shell = True)
 

e05e7914c589e6d73b925a2246566a7150a722e2
Jet <jethro0610@outlook.com>
2025-05-11 17:27:37 -0700

    Ease

---
+++ Source/Helpers/EaseType.h
@@ -0,0 +1,16 @@
+#pragma once
+enum EaseType {
+    E_Linear,
+
+    E_Quad,
+    E_InQuad,
+    E_OutQuad,
+
+    E_Cubic,
+    E_InCubic,
+    E_OutCubic,
+
+    E_Elastic,
+    E_InElastic,
+    E_OutElastic
+};

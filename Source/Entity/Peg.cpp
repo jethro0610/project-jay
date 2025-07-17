c449a2a37ab0f2fffee99f28dc0aa9443e645951
Jet <jethro0610@outlook.com>
2025-07-16 19:54:58 -0700

    Hop pegs and homing

--- Source/Entity/Peg.cpp
+++ Source/Entity/Peg.cpp
@@ -1,6 +1,7 @@
 #include "Peg.h"
 #include "Resource/ResourceManager.h"
 #include "Player.h"
+#include "Seed/SeedManager.h"
 using namespace glm;
 
 EntityDependendies Peg::GetStaticDependencies() {
@@ -13,9 +14,11 @@ EntityProperties Peg::GetStaticProperties() {
     return {
         {
             // Floats
+            {"p_jump", &jumpStrength_}
         },
         {
             // Ints
+            {"p_seeds", &seeds_}
         },
         {
             // Bools
@@ -37,17 +40,42 @@ void Peg::Init(Entity::InitArgs args) {
     pushbox_.bottom = 1.0f;
     pushbox_.radius = 1.0f;
 
-    overlapbox_.top = 1.0f;
-    overlapbox_.bottom = 1.0f;
-    overlapbox_.radius = 1.0f;
+    overlapbox_.top = 1.25f;
+    overlapbox_.bottom = -0.75f;
+    overlapbox_.radius = 1.25f;
 
     SetFlag(EF_Homeable, true);
     SetFlag(EF_Overlap, true);
+    SetFlag(EF_SendPush, true);
+    seeds_ = 0;
+    jumpStrength_ = 100.0f;
+}
+
+void Peg::OnDestroy() {
+    seedManager_->CreateMultipleSeed(transform_.position, seeds_, 20.0f, lastOverlap_);
 }
 
 void Peg::OnOverlap(Entity* overlappedEntity) {
     overlappedEntity->skipGroundCheck_ = true;
 
-    if (overlappedEntity->typeId_ == Player::TYPEID)
-        ((Player*)overlappedEntity)->EndHoming();
+    if (overlappedEntity->typeId_ != Player::TYPEID)
+        return;
+
+    lastOverlap_ = overlappedEntity;
+
+    Player* player = (Player*)overlappedEntity;
+    player->EndHoming();
+    if (player->onGround_)
+        return;
+    if (player->velocity_.y > 0.0f && player->homingTarget_ == nullptr)
+        return;
+
+    player->velocity_.y = jumpStrength_;
+    player->hitlag_ = 4;
+    hitlag_ = 4;
+    destroy_ = true;
+}
+
+vec3 Peg::GetTargetPoint() {
+    return transform_.position + vec3(0.0f, transform_.scale.y, 0.0f);
 }
--- Source/Entity/Peg.h
+++ Source/Entity/Peg.h
@@ -4,6 +4,8 @@
 class Peg : public Entity {
 public:
     void Init(InitArgs args);
+    void OnDestroy();
+    int GetSeeds() { return seeds_; }
 
     static TypeID TYPEID;
     static const char* GetStaticName() { return "e_peg"; }
@@ -11,4 +13,10 @@ public:
     EntityProperties GetStaticProperties();
 
     void OnOverlap(Entity* overlappedEntity);
+
+    Entity* lastOverlap_;
+    int seeds_;
+    float jumpStrength_;
+
+    glm::vec3 GetTargetPoint();
 };
